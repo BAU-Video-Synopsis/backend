706f81f6774312b36bbba0eb53c59d5609de0bdf
Ekrem Bulut <109585432+EkremBulut01@users.noreply.github.com>
2024-05-27 05:08:05 +0300

    .

--- index.js
+++ index.js
@@ -1,113 +1,112 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const ffmpeg = require('fluent-ffmpeg');
-const path = require('path');
-const { UploadedVideo, User } = require('./schemas/schemas');
-const bcrypt = require('bcrypt');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const ffmpeg = require("fluent-ffmpeg");
+const path = require("path");
+const { UploadedVideo, User } = require("./schemas/schemas");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
 const app = express();
 const port = 3001;
-const axios = require('axios');
+const axios = require("axios");
 app.use(cors());
-mongoose.connect('mongodb+srv://sarpertatkoy:Dce2208SCtgCNPuL@capstone.aajahuz.mongodb.net/Capstone');
+mongoose.connect(
+  "mongodb+srv://sarpertatkoy:Dce2208SCtgCNPuL@capstone.aajahuz.mongodb.net/Capstone"
+);
 
 app.use(bodyParser.json());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, 'uploads'));
+    cb(null, path.join(__dirname, "uploads"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
-  }
+  },
 });
 
 const upload = multer({ storage: storage });
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.post('/uploadVideo', upload.single('video'), async (req, res) => {
+app.post("/uploadVideo", upload.single("video"), async (req, res) => {
   try {
-    const filePath = path.join(__dirname, 'uploads', req.file.filename);
+    const filePath = path.join(__dirname, "uploads", req.file.filename);
     const compressedFileName = `compressed_${req.file.filename}`;
-    const compressedFilePath = path.join(__dirname, 'uploads', compressedFileName);
-
+    const compressedFilePath = path.join(__dirname, "uploads", compressedFileName);
+    const isLogined = req.isLogined
     ffmpeg(filePath)
       .output(compressedFilePath)
-      .videoCodec('libx264')
-      .on('end', async () => {
+      .videoCodec("libx264")
+      .on("end", async () => {
         ffmpeg.ffprobe(compressedFilePath, async (err, metadata) => {
           if (err) {
-            console.error('Video boyutu alınamadı:', err);
-            return res.status(500).json({ error: 'Error getting video size' });
+            console.error("Video boyutu alınamadı:", err);
+            return res.status(500).json({ error: "Error getting video size" });
           }
-
-          const { width, height } = metadata.streams[0];
           const sizeInBytes = metadata.format.size;
           const duration = metadata.format.duration;
+          const url = "http://localhost:5000/data";
+          const data = {
+            filePath: compressedFilePath,
+          };
 
-          const newVideo = new UploadedVideo({
-            name: 'enyenivideo',
+          axios
+            .post(url, data)
+            .then(async (response) => {
+              console.log(response.data);
+              res.status(200).json({ message: "Video successfully uploaded and data sent" });
+              if(isLogined) {
+                 const newVideo = new UploadedVideo({
+            name: compressedFileName,
             time: duration,
             size: sizeInBytes,
-            videoUrl: `/uploads/${compressedFileName}`
+            videoUrl: `/uploads/${compressedFileName}`,
           });
 
           await newVideo.save();
-          const url = 'http://localhost:5000/data';
-const data = {
-    name: 'Jane Doe',
-    filePath : compressedFilePath
-};
-
-axios.post(url, data)
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-         
+              } 
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+              res.status(500).json({ error: "Error sending data to API" });
+            });
         });
-      })
-      .on('error', (err) => {
-        console.error('Error compressing video:', err);
-        res.status(500).json({ error: 'Error compressing video' });
       })
       .run();
   } catch (error) {
-    console.error('Error uploading video:', error);
-    res.status(500).json({ error: 'Error uploading video' });
+    console.error("Error uploading video:", error);
+    res.status(500).json({ error: "Error uploading video" });
   }
 });
 
+
 // User registration endpoint
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email) {
-    return res.status(400).send('Email is required');
+    return res.status(400).send("Email is required");
   }
 
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ email, password: hashedPassword });
     await user.save();
-    res.status(201).send('User registered successfully');
+    res.status(201).send("User registered successfully");
   } catch (error) {
     if (error.code === 11000) {
       console.log(error);
-      res.status(400).send('Email already exists');
+      res.status(400).send("Email already exists");
     } else {
-      res.status(500).send('Internal server error');
+      res.status(500).send("Internal server error");
     }
   }
 });
 
 // User login endpoint
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
@@ -132,7 +131,7 @@ app.get("/getVideos", async (req, res) => {
     res.json(videos);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'An error occurred while fetching videos.' });
+    res.status(500).json({ error: "An error occurred while fetching videos." });
   }
 });
 
